94c58b3362908a432349c28f95ed6aa8ca037238
Alek30k <alemank23k@gmail.com>
2023-07-26 14:35:20 -0300

    first commit

---
+++ src/components/Notification.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
+      Entrega gratuita para todos los pedidos superiores a $ 50. Pide tu comida
+      ahora!
+    </div>
+  );
+};
+
+export default Notification;
